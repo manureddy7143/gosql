2c2708711b65504bc7ea223c1765665c2e8c8c03
manu <manureddy.com>
2020-11-06 19:30:58 +0530

    modified errors and updated new queries.

--- mysql.go
+++ mysql.go
@@ -7,10 +7,13 @@ import (
   "encoding/json"
   "fmt"
   "io/ioutil"
+
+  
 )
 type Post struct {
   ID string `json:"id"`
-  Title string `json:"title"`
+  Title string `json:"name"`
+  CITY string `json:"city"`
 }
 var db *sql.DB
 var err error
@@ -22,8 +25,8 @@ db, err = sql.Open("mysql", "root:143114@tcp(127.0.0.1:3306)/test")
   defer db.Close()
   router := mux.NewRouter()
   router.HandleFunc("/posts", getPosts).Methods("GET")
-//   router.HandleFunc("/posts", createPost).Methods("POST")
-//   router.HandleFunc("/posts/{id}", getPost).Methods("GET")
+  router.HandleFunc("/posts", createPost).Methods("POST")
+  router.HandleFunc("/posts/{id}", getPosts).Methods("GET")
 //   router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
 //   router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
   http.ListenAndServe(":8080", router)
@@ -31,14 +34,15 @@ db, err = sql.Open("mysql", "root:143114@tcp(127.0.0.1:3306)/test")
 func getPosts(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
   var posts []Post
-  result, err := db.Query("SELECT id, title from posts")
+  result, err := db.Query("SELECT id, name, city from employee")
+  fmt.Print(err,result)
   if err != nil {
     panic(err.Error())
   }
   defer result.Close()
   for result.Next() {
     var post Post
-    err := result.Scan(&post.ID, &post.Title)
+    err := result.Scan(&post.ID, &post.Title, &post.CITY)
     if err != nil {
       panic(err.Error())
     }
@@ -46,72 +50,30 @@ func getPosts(w http.ResponseWriter, r *http.Request) {
   }
   json.NewEncoder(w).Encode(posts)
 }
-// func createPost(w http.ResponseWriter, r *http.Request) {
+func createPost(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
-  stmt, err := db.Prepare("INSERT INTO posts(title) VALUES(?)")
+  stmt, err := db.Prepare("INSERT INTO employee(id,name,city) VALUES(?,?,?)")
+  fmt.Println(err,stmt)
   if err != nil {
     panic(err.Error())
   }
   body, err := ioutil.ReadAll(r.Body)
+  fmt.Print(err,body)
   if err != nil {
     panic(err.Error())
   }
   keyVal := make(map[string]string)
   json.Unmarshal(body, &keyVal)
-  title := keyVal["title"]
-  _, err = stmt.Exec(title)
+  fmt.Println(keyVal,stmt)
+  id:=keyVal["id"]
+  name:=keyVal["name"]
+  city:=keyVal["city"]
+  _, err = stmt.Exec(id,name,city)
+  fmt.Println(err)
   if err != nil {
     panic(err.Error())
   }
   fmt.Fprintf(w, "New post was created")
-}
-// func getPost(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
-  params := mux.Vars(r)
-  result, err := db.Query("SELECT id, title FROM posts WHERE id = ?", params["id"])
-  if err != nil {
-    panic(err.Error())
   }
-  defer result.Close()
-  var post Post
-  for result.Next() {
-    err := result.Scan(&post.ID, &post.Title)
-    if err != nil {
-      panic(err.Error())
-    }
-  }
-  json.NewEncoder(w).Encode(post)
-}
-// func updatePost(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
-  params := mux.Vars(r)
-  stmt, err := db.Prepare("UPDATE posts SET title = ? WHERE id = ?")
-  if err != nil {
-    panic(err.Error())
-  }
-  body, err := ioutil.ReadAll(r.Body)
-  if err != nil {
-    panic(err.Error())
-  }
-  keyVal := make(map[string]string)
-  json.Unmarshal(body, &keyVal)
-  newTitle := keyVal["title"]
-  _, err = stmt.Exec(newTitle, params["id"])
-  if err != nil {
-    panic(err.Error())
-  }
-  fmt.Fprintf(w, "Post with ID = %s was updated", params["id"])
-}
-// func deletePost(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
-  params := mux.Vars(r)
-  stmt, err := db.Prepare("DELETE FROM posts WHERE id = ?")
-  if err != nil {
-    panic(err.Error())
-  }
-  _, err = stmt.Exec(params["id"])
-  if err != nil {
-    panic(err.Error())
-  }
-  fmt.Fprintf(w, "Post with ID = %s was deleted", params["id"])
-}
+
+
